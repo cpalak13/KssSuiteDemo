299487d2ec6efea6252d0e2f61990574df657927
Chaudhary <palak.chaudhary@accenture.com>
2020-05-14 21:57:20 +0530

    Changed path in bat file

--- TestProject/testProjectBat.bat
+++ TestProject/testProjectBat.bat
@@ -1,5 +1,5 @@
-set projectLocation=
+set projectLocation=C:\Users\palak.chaudhary\.jenkins\workspace\KssPipelineDemo\TestProject
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\*
-java org.testng.TestNG %projectLocation%\testng.xml
+java org.testng.TestNG %projectLocation%\testNG.xml
 pause
